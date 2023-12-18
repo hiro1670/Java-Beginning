827838bea8680019d173c21119c7682d0d60f626
hiro1670 <hiroto0414@ezweb.ne.jp>
2023-12-18 17:47:40 +0900

    [2章-キーボードから1行の入力を受け取る命令]

--- Java-Beginning/src/Sample/main.java
+++ Java-Beginning/src/Sample/main.java
@@ -4,7 +4,12 @@ public class main {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-		System.out.println("Hello Java");
+		System.out.println("あなたの名前を入力してください。");
+		String name = new java.util.Scanner(System.in).nextLine();
+		System.out.println("あなたの年齢を入力してください。");
+		int age = new java.util.Scanner(System.in).nextInt();
+		System.out.println
+		("ようこそ、" + age + "歳の" + name + "さん");
 	}
 
 }
