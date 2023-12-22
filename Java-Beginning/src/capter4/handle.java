69baf93bf13f36e8ac4e4eac11456499e7e9c221
hiro1670 <hiroto0414@ezweb.ne.jp>
2023-12-22 17:11:31 +0900

    [4章-配列]

---
+++ Java-Beginning/src/capter4/handle.java
@@ -0,0 +1,12 @@
+package capter4;
+
+public class handle {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int[] scores = {20, 30, 40, 50, 80};
+		for (int valse : scores) {
+			System.out.println(valse);
+		}
+	}
+}
