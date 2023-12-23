68b3ccfda584dbb76191afd667825584959e56d9
hiro1670 <hiroto0414@ezweb.ne.jp>
2023-12-23 18:19:55 +0900

    [Finish]practise4-2

---
+++ Java-Beginning/src/practice4/practice2.java
@@ -0,0 +1,16 @@
+package practice4;
+
+public class practice2 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int[] moneyList = new int[] {121902, 8302, 55100};
+		for (int i = 0; i < moneyList.length; i++) {
+			System.out.println(moneyList[i]);
+		}
+		for (int value: moneyList) {
+			System.out.println(value);
+		}
+	}
+
+}
