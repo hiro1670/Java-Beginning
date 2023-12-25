2e598a2a2f40b5c928a7ce20203284e43fa4170e
hiro1670 <hiroto0414@ezweb.ne.jp>
2023-12-25 14:08:34 +0900

    [Finish]5.3 戻り値の利用

---
+++ Java-Beginning/src/capter5/asIs.java
@@ -0,0 +1,14 @@
+package capter5;
+
+public class asIs {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println(add(add(10, 20), add(30, 40)));
+	}
+	public static int add(int x, int y) {
+		int ans = x + y;
+		return ans;
+	}
+
+}
---
+++ Java-Beginning/src/capter5/returnValue.java
@@ -0,0 +1,15 @@
+package capter5;
+
+public class returnValue {
+
+	public static int add(int x, int y) {
+		// TODO 自動生成されたメソッド・スタブ
+		int ans = x + y;
+		return ans;
+	}
+	public static void main(String[] args) {
+		int ans = add(100, 10);
+		System.out.println("100 + 10 = " + ans);
+	}
+
+}
