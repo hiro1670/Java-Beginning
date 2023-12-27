1b2486b2185fd6a47fe6064f0fabdaee8eb24f82
hiro1670 <hiroto0414@ezweb.ne.jp>
2023-12-27 15:49:48 +0900

    [Finish]6.1 ソースファイルを分割する

---
+++ Java-Beginning/src/capter6_1/CalcLogic.java
@@ -0,0 +1,13 @@
+package capter6_1;
+
+public class CalcLogic {
+	
+	public static int tasu(int a, int b) {
+		return (a + b);
+	}
+	
+	public static int hiku(int a, int b) {
+		return (a - b);
+	}
+
+}
---
+++ Java-Beginning/src/capter6_1/Split.java
@@ -0,0 +1,12 @@
+package capter6_1;
+
+public class Split {
+	
+	public static void main(String[] args) {
+		int a = 10; int b = 2;
+		int total = CalcLogic.tasu(a, b);
+		int delta = CalcLogic.hiku(a, b);
+		System.out.println("足すと" + total + "、引くと" + delta);
+	}
+
+}
