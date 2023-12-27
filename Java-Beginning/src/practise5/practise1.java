11533662e5f9b98917a0a90a543b1e3af954e889
hiro1670 <hiroto0414@ezweb.ne.jp>
2023-12-27 14:05:05 +0900

    [Finish]5.1 練習問題

---
+++ Java-Beginning/src/practise5/practise1.java
@@ -0,0 +1,17 @@
+package practise5;
+
+public class practise1 {
+	
+	public static void main(String[] args) {
+		introduceOneself();
+	}
+	
+	public static void introduceOneself() {
+		String name = "湊雄輔";
+		int age = 22;
+		double height = 169.9;
+		char zodiac = '辰';
+		System.out.println
+		("私の名前は" + name + "です。歳は" + age + "歳です。身長は" + height + "cmです。十二支は" + zodiac + "です。");
+	}
+}
