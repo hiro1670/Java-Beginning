f16ae3e4f4f93ea8cc1e2f52472b8d7fe4953ba0
hiro1670 <hiroto0414@ezweb.ne.jp>
2024-01-20 17:52:22 +0900

    [Finish]8.2.5 操作の宣言方法

--- Java-Beginning/src/capter8/Hero.java
+++ Java-Beginning/src/capter8/Hero.java
@@ -1,5 +1,13 @@
 package capter8;
 
 public class Hero {
-
+	String name;
+	
+	int hp;
+	
+	public void attack() {}
+	public void sleep() {}
+	public void sit(int sec) {}
+	public void slip() {}
+	public void run() {}
 }
