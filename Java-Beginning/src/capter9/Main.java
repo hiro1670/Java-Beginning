533a1f9f10138a9ead8afba839dab5f676d0fbb8
hiro1670 <hiroto0414@ezweb.ne.jp>
2024-02-06 17:55:14 +0900

    [Finish]9.2.5 2つ以上の同名のコンストラクタを定義する

--- Java-Beginning/src/capter9/Main.java
+++ Java-Beginning/src/capter9/Main.java
@@ -1,32 +1,16 @@
 package capter9;
 
+import capter8.Hero;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-		Hero h1 = new Hero();
-		
-		h1.name = "ミナト";
-		
-		h1.hp = 100;
+		Hero h1 = new Hero("ミナト");
+		System.out.println(h1.name);
 		
 		Hero h2 = new Hero();
-		
-		h2.name = "アサカ";
-		
-		h2.hp = 100;
-		
-		Wizard w = new Wizard();
-		
-		w.name = "スガワラ";
-		
-		w.hp = 50;
-		
-		w.heal(h1);
-		
-		w.heal(h2);
-		
-		w.heal(h2);
+		System.out.println(h2.name);
 	}
 
 }
--- Java-Beginning/src/capter9/Sword.java
+++ Java-Beginning/src/capter9/Sword.java
@@ -5,16 +5,7 @@ public class Sword {
 	String name;
 	
 	int damage;
-}
 
-public class Hero {
-	
-	String name;
-	
-	int hp;
-	
-	Sword sword;
-	
 	public void attack() {
 		System.out.println(this.name + "は、攻撃した！");
 		System.out.println("敵に5ポイントのダメージをあたえた！");
